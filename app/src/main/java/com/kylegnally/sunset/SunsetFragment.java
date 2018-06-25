de1cc33b4e1280da0f2344425759b4ce3c074158
Kyle Nally <kylegnally@gmail.com>
2018-06-25 11:03:48 -0400

    Add sun, sky, and sea colors to colors.xml. Create fragment_sunset layout. Create SunsetFragment.java. Modify SunsetActivity to create instance of SunsetFragment.

---
+++ app/src/main/java/com/kylegnally/sunset/SunsetFragment.java
@@ -0,0 +1,25 @@
+package com.kylegnally.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+        return view;
+    }
+}
